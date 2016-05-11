c5e53ecad4793c0ab68ae5ebb4bbff97c54f2050
T8y8 <doyle.tyler@gmail.com>
2016-05-10 17:51:12 -0700

    Added new test cases

--- Document API/test.py
+++ Document API/test.py
@@ -4,11 +4,13 @@ from tableaudocumentapi import Workbook
 
 class HelperMethodTests(unittest.TestCase):
 
-    def test_valid_file_with_valid_inputs(self):
+    def test_is_valid_file_with_valid_inputs(self):
         self.assertTrue(Workbook._is_valid_file('file1.tds'))
         self.assertTrue(Workbook._is_valid_file('file2.twb'))
+        self.assertTrue(Workbook._is_valid_file('tds.twb'))
 
-    def test_valid_file_with_invalid_inputs(self):
+    def test_is_valid_file_with_invalid_inputs(self):
+        self.assertFalse(Workbook._is_valid_file(''))
         self.assertFalse(Workbook._is_valid_file('file1.tds2'))
         self.assertFalse(Workbook._is_valid_file('file2.twb3'))
 
