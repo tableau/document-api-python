c389609d1cf7c59c5496961c328fe1d31ebffe04
Russell Hay <me@russellhay.com>
2016-07-11 10:07:50 -0700

    Adding field count and numbering

--- Examples/GetFields/show_fields.py
+++ Examples/GetFields/show_fields.py
@@ -12,14 +12,18 @@ sourceTDS = Datasource.from_file('World.tds')
 # Step 3)  Print out all of the fields and what type they are
 ############################################################
 print('----------------------------------------------------------')
+print('--- {} total fields in this datasource'.format(len(sourceTDS.fields)))
+print('----------------------------------------------------------')
+count = 0
 for field_key, field in sourceTDS.fields.items():
-    print('{} is a {}'.format(field.name, field.datatype))
+    count += 1
+    print('{:>4}: {} is a {}'.format(count, field.name, field.datatype))
     blank_line = False
     if field.calculation:
-        print('--- the formula is {}'.format(field.calculation))
+        print('      the formula is {}'.format(field.calculation))
         blank_line = True
     if field.aggregation:
-        print('--- the default aggregation is {}'.format(field.aggregation))
+        print('      the default aggregation is {}'.format(field.aggregation))
         blank_line = True
 
     if blank_line:
