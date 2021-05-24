ceb78e342f70b5d82c30374d2cd7b8e45dd37112
Daniel Teo <daniel.teo@datavizardry.com>
2021-05-24 16:50:20 +0200

    create DatasourceDependancy object and add to Worksheet

--- tableaudocumentapi/__init__.py
+++ tableaudocumentapi/__init__.py
@@ -1,5 +1,6 @@
 from .field import Field
 from .filter import Filter
+from .ds_dependancy import DatasourceDependancy
 from .connection import Connection
 from .datasource import Datasource, ConnectionParser
 from .worksheet import Worksheet
