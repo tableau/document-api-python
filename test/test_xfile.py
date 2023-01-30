2af93b585d3616d2f10ac691d5ac0bf583cb252f
Stu Tomlinson <stu@nosnilmot.com>
2023-01-30 07:06:07 +0000

    Do not parse XML twice when opening packaged files (#245)

--- test/test_xfile.py
+++ test/test_xfile.py
@@ -24,11 +24,13 @@ TWBX_WITH_CACHE_FILES = os.path.join(
 class XFileEdgeTests(unittest.TestCase):
     def test_find_file_in_zip_no_xml_file(self):
         badzip = zipfile.ZipFile(BAD_ZIP_FILE)
-        self.assertIsNone(find_file_in_zip(badzip))
+        xml_file, xml_tree = find_file_in_zip(badzip)
+        self.assertIsNone(xml_file)
 
     def test_only_find_twbs(self):
         twb_from_twbx_with_cache = zipfile.ZipFile(TWBX_WITH_CACHE_FILES)
-        self.assertEqual(find_file_in_zip(twb_from_twbx_with_cache), 'Superstore.twb')
+        xml_file, xml_tree = find_file_in_zip(twb_from_twbx_with_cache)
+        self.assertEqual(xml_file, 'Superstore.twb')
 
 
 class Namespacing(unittest.TestCase):
