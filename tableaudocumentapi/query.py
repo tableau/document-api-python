bd373059ddf1db13d0173f0918a2461a6ec9a1cf
Justin Bisal <justin.s.bisal@gmail.com>
2025-08-24 01:09:45 -0400

    added get_workbook_dependencies query

--- tableaudocumentapi/query.py
+++ tableaudocumentapi/query.py
@@ -4,37 +4,39 @@ class Query(object):
     def __init__(self, workbook):
         self._workbook = workbook
         self._xml = workbook._workbookRoot
+        self._worksheet_dashboard_map = self._get_worksheet_dashboard_map()
     
         
-    def get_dashboards_for_worksheet(self, worksheet_name):
-        dashboards = []
-        for _ in self._workbook.dashboard_objects.values():
-            if worksheet_name in _.worksheets:
-                dashboards.append(_.name)
-        return dashboards
+    def _get_worksheet_dashboard_map(self):
+        worksheet_dashboard_map = {}
+        for this_worksheet in self._workbook.worksheet_objects:
+            worksheet_dashboard_map[this_worksheet] = []
+            for dashboard, worksheets_in_dashboard in self._workbook.dashboard_objects.items():
+                import pdb; pdb.set_trace()
+                if this_worksheet in worksheets_in_dashboard.worksheets:
+                    worksheet_dashboard_map[this_worksheet].append(dashboard)
+        return worksheet_dashboard_map
+    
 
     
-    def get_workbook_depenencies(self):
+    def get_workbook_dependencies(self):
         workbook_dependencies = []
         for worksheet in self._workbook.worksheet_objects.values():
             # import pdb; pdb.set_trace()
-            if len(self.get_dashboards_for_worksheet(worksheet.name)) == 0:
-                dbs = [None]
-            else:
-                dbs = self.get_dashboards_for_worksheet(worksheet.name)
-            for db in dbs:
-                for dependency in worksheet.datasource_dependencies:
-                    for column in dependency.columns.values():
-                        # import pdb; pdb.set_trace()
-                        workbook_dependencies.append({
-                            "Workbook":self._workbook.filename,
-                            "Dashboard":db,
-                            "Worksheet":worksheet.name,
-                            "Datasource":dependency.datasource,
-                            "Field":column.get('name'),
-                            "Caption":column.get('caption'),
-                            "Datatype":column.get('datatype'),
-                            "Role":column.get('role'),
-                            "Type":column.get('type')
-                        })
+            for dependency in worksheet.datasource_dependencies:
+                for column in dependency.columns.values():
+                    # import pdb; pdb.set_trace()
+                    workbook_dependencies.append({
+                        "Workbook":self._workbook.filename,
+                        "Dashboard":self._worksheet_dashboard_map.get(worksheet.name),
+                        "Worksheet":worksheet.name,
+                        "Datasource":dependency.datasource,
+                        "Field":column.get('name'),
+                        "Caption":column.get('caption'),
+                        "Datatype":column.get('datatype'),
+                        "Role":column.get('role'),
+                        "Type":column.get('type')
+                    })
         return workbook_dependencies
+    
+    
