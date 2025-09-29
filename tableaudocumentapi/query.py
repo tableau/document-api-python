29096bba5fcf2ae97d24696b74e0faec98c41b8c
jbisal <justin.s.bisal@gmail.com>
2025-09-28 21:31:36 -0600

    Added normalize_filter to query object

--- tableaudocumentapi/query.py
+++ tableaudocumentapi/query.py
@@ -9,7 +9,6 @@ class Query(object):
         self._workbook = workbook
         self._xml = workbook._workbookRoot
         self._worksheet_dashboard_map = self._get_worksheet_dashboard_map()
-    
         
     def _get_worksheet_dashboard_map(self):
         worksheet_dashboard_map = {}
@@ -20,9 +19,7 @@ class Query(object):
                     worksheet_dashboard_map[this_worksheet].append(dashboard)
             worksheet_dashboard_map[this_worksheet] = worksheet_dashboard_map[this_worksheet]
         return worksheet_dashboard_map
-    
 
-    
     def get_worksheet_dependencies(self):
         worksheet_dependencies = []
         for worksheet in self._workbook.worksheet_objects.values():
@@ -41,21 +38,33 @@ class Query(object):
                     })
         return worksheet_dependencies
     
-    
-    
     def get_worksheet_filters(self):
-        workbook_filters = []
+        worksheet_filters = []
         for worksheet in self._workbook.worksheet_objects.values():
             for filter_obj in worksheet.filters:
-                workbook_filters.append({
+                worksheet_filters.append({
                     "Worksheet": worksheet.name,
                     "Filter_class": filter_obj.filter_class,
                     "Datasource": filter_obj.datasource,
                     "Column": filter_obj.column,
                     "Groupfilters": filter_obj.groupfilters
                 })
-        return workbook_filters
+        return worksheet_filters
     
+    def normalize_groupfilter(self, filter_json):    
+        # import pdb; pdb.set_trace()
+        normalized_groupfilter = pd.json_normalize(filter_json).explode("children").reset_index(drop=True).to_dict(orient="records")
+        return normalized_groupfilter
+    
+    def normalize_worksheet_filters(self, worksheet_filters):
+        for worksheet_filter in worksheet_filters:
+            if len(worksheet_filter.get('Groupfilters')) > 0:
+                worksheet_filter['normalized_groupfilter'] = self.normalize_groupfilter(worksheet_filter.get('Groupfilters'))
+        worksheet_filters = pd.DataFrame(worksheet_filters).explode('normalized_groupfilter')
+        worksheet_normalized_groupfilter = pd.json_normalize(worksheet_filters['normalized_groupfilter'])
+        worksheet_filters = worksheet_filters.join(worksheet_normalized_groupfilter)
+        return worksheet_filters
+            
     def get_worksheet_rows(self):
         worksheet_rows = []
         for worksheet in self._workbook.worksheet_objects.values():
@@ -78,13 +87,10 @@ class Query(object):
                 })
         return worksheet_cols
     
-    
-    
     def get_field_objects(self, column, datasource_name = None):
         """Link filter column or worksheets rows/cols to actual Field object from datasource"""
         if not isinstance(column, str) or not column:
             return None
-
         if datasource_name is None:
             # Extract field name from column reference
             # '[federated.xxx].[Table]' -> 'Table'
@@ -122,8 +128,6 @@ class Query(object):
                     })
         return workbook_parameters
     
-    
-    
     def get_workbook_fields(self):
         """Get all non-parameter Fields in a workbook and their attributes as a list of dictionaries"""
         field_attributes = [
@@ -137,6 +141,7 @@ class Query(object):
                 if key.startswith('[') and key.endswith(']'):
                     field_dict = {}
                     field_dict['datasource'] = datasource.name
+                    field_dict['datasource_caption'] = datasource.caption
                     field_dict['field_key'] = key
                     for field_attribute in field_attributes:
                         field_dict[field_attribute] = getattr(fields[key],field_attribute)
@@ -145,32 +150,27 @@ class Query(object):
     
     def get_workbook_diff_table(self):
         """Generate a table all workbook dependencies and their attributes"""
-        # Join Worksheets with dashboards
-        import pdb; pdb.set_trace()
+        # Join Worksheets with dashboards        
         df_dependencies = pd.DataFrame(self.get_worksheet_dependencies())
         worksheet_dashboard_map = self._get_worksheet_dashboard_map()
         df_worksheet_dashboard_map = pd.DataFrame({"Worksheet":worksheet_dashboard_map.keys(), "Dashboard":worksheet_dashboard_map.values()})
         df_diff = df_dependencies.merge(df_worksheet_dashboard_map, how='left', on='Worksheet')
         
-        
         # Join with Filters
         df_filters = pd.DataFrame(self.get_worksheet_filters())
         df_diff = pd.merge(df_diff, df_filters.add_prefix('_filter_'), left_on = ['Datasource', 'Column_instance', 'Worksheet'], 
                            right_on=['_filter_Datasource', '_filter_Column', '_filter_Worksheet'], how='left')
-        
         # Join with Rows
         df_rows = pd.DataFrame(self.get_worksheet_rows())
         df_diff = pd.merge(df_diff, df_rows.add_prefix('_rows_'), left_on = ['Datasource', 'Column_instance', 'Worksheet'], 
                            right_on=['_rows_Datasource', '_rows_Row', '_rows_Worksheet'], how='left')
-        
         # Join with Cols
         df_cols = pd.DataFrame(self.get_worksheet_cols())
         df_diff = pd.merge(df_diff, df_cols.add_prefix('_cols_'), left_on = ['Datasource', 'Column_instance', 'Worksheet'], 
                            right_on=['_cols_Datasource', '_cols_Col', '_cols_Worksheet'], how='left')
-        
         # Join with fields
         df_fields = pd.DataFrame(self.get_workbook_fields())
         df_diff = pd.merge(df_diff, df_fields.add_prefix('_fields_'), left_on = ['Datasource', 'Column_instance'], 
                            right_on=['_fields_datasource', '_fields_field_key'], how='left')
-    
+            
         return df_diff
