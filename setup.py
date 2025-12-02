991c199c047398d1bac6330af50f11afe2ef6ac6
jbisal <justin.s.bisal@gmail.com>
2025-12-02 16:34:43 -0700

    Changed mcp server to stdio and moved to root

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup
-    
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='tableaudocumentapi',
     version='0.12',
-    author='Tableau',
+    author='Justin Bisal',
     author_email='justin@viz-explainer.com',
     url='https://github.com/jbisal/document-api-python',
     packages=['tableaudocumentapi'],
     license='MIT',
-    description='A Python module for working with Tableau files.',
-    long_description="file: README.md",
+    description='A Python module for working with Tableau files, with version-control diff engine and MCP integration.',
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires='>=3.10',
     test_suite='test',
-    install_requires=['lxml', 'pandas'],
+    install_requires=['lxml', 'pandas', 'fastmcp'],
     entry_points={
         'console_scripts': [
-            'twb-diff = tableaudocumentapi.cli:main',
+            'twb-diff=tableaudocumentapi.cli:main',
         ],
     }
 )
